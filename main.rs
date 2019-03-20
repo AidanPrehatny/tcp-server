da51be89cc4a74dfab9e0660f99377309ec0a100
Aidan Prehatny <aprehatny@yahoo.com>
2019-03-20 01:48:16 -0700

    add bin to toml, create const for tcp ip

--- main.rs
+++ main.rs
@@ -6,13 +6,14 @@ fn handle_client(stream: TcpStream) {
     println!("{:?}", stream);
 }
 
+const TCPLISTENER: &str = "127.0.0.1:5060";
 fn main() -> io::Result<()> {
-    let listener = TcpListener::bind("127.0.0.1:5060")?;
+    let listener = TcpListener::bind(TCPLISTENER)?;
 
+    println!("TCP Server listening on: {}", TCPLISTENER);
     // accept connecitons and process them serially
     for stream in listener.incoming() {
         handle_client(stream?);
     }
-    println!("Hello, world!");
     Ok(())
 }
